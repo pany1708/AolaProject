4e2eb692798808d3f767848c761458a1b19a4246
unknown <weijuntao@rd.com>
2017-08-15 18:45:15 +0800

    20170815

---
+++ Function接口.java
@@ -0,0 +1,14 @@
+1. java8中java.util.function包下的函数式编程接口.
+
+这个接口的主要的方法:
+apply(): 将Function对象应用到输入的参数上.
+
+
+2. Java中重要的函数接口:
+
+  Predicate<T>
+  Consume<T>
+  Function<T,R>
+  Supplies<T>
+  UnaryOperator<T>
+  BinaryOperator<T>
---
+++ java8新特性.java
@@ -0,0 +1,22 @@
+1. [菜鸟官网](http://www.runoob.com/java/java8-new-features.html)
+
+1) Lambda
+2) 方法引用
+3) 默认方法
+4) 新工具：新的编译工具，如：Nashorn引擎 jjs、 类依赖分析器jdeps
+5) Stream API
+6) Data Time API
+7) Options类：用来解决空指针异常
+8) Nashorn, JavaScript 引擎 − Java 8提供了一个新的Nashorn javascript引擎，它允许我们在JVM上运行特定的javascript应用
+
+大多数都出现在 java.util包下
+
+Stream是并发
+
+2. 并行增强：并行数组 Arrays.parallelXXX(): 并行处理,并且可以指定范围
+eg:
+parallelPrefix将数组中每个元素替换为指定关联操作前缀的积累
+
+假设array [1, 2, 3, 4, ...]，执行完Arrays.parallelPrefix(values, (x, y) -> x * y)之后，array的结果为
+
+[1, 1 × 2, 1 × 2 × 3, 1 × 2 × 3 × 4, ...]
