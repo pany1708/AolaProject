42829af6b967e5fdaeb1ccca483a53f0776dcabb
unknown <weijuntao@rd.com>
2017-10-27 20:09:25 +0800

    update

--- rmi/rmi.java
+++ rmi/rmi.java
@@ -1,3 +1,13 @@
 1. 远程方法调用(Remote Method Invocation),能够让jvm上的对象像调用本地对象一样调用另一个jvm上的对象的方法.
 
-2. rmi客户端可以根据服务端暴露的接口,就可以去实现调用rmi服务端的代码实现。感觉就像是在调用自己本地的代码一样。但实际上是在调用远程机器上的代码。 
+2. rmi客户端可以根据服务端暴露的接口,就可以去实现调用rmi服务端的代码实现。感觉就像是在调用自己本地的代码一样。但实际上是在调用远程机器上的代码。
+
+3. RPC(remote procedure call protocal)
+
+4. PRC与PMI:
+   1) RPC跨语言,而PMI仅仅支持Java.
+   2) PMI调用远程对象方法,RPC不支持对象的概念,可以说是:PMI是面向对象方式的JavaRPC.
+
+5. JMS(Java Messaging Service),JMS是Java的消息服务.
+
+6. 
