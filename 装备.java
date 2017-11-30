823d38e0e795806258e606afa5f297088d467b41
unknown <weijuntao@rd.com>
2017-12-01 00:39:41 +0800

    update

--- 装备.java
+++ 装备.java
@@ -2,3 +2,6 @@
 cardList = PetCardService.instance.getBatchUserPetCards(userId,
 					StringUtil.intArrayToString(EQUIP_ID_CONFIG[i]));
 equipList = PetEquipService.getInstance().getBatchUserPetExistCardsByEquipTypes(userId, EQUIP_ID_CONFIG[i]);
+
+
+2. // colors是颜色0-4白绿蓝紫金
