424801f33ff36db4e4827b7e5cf9e7da59b4572a
unknown <weijuntao@rd.com>
2017-12-16 18:40:49 +0800

    update 17121601

--- 统计.java
+++ 统计.java
@@ -8,3 +8,27 @@ eventId=24193
 
 
 buttonId 需要指定
+
+
+1. LogParticipationHandler.log(u, eventId);
+
+2. 将统计里的字段拷贝进EventIdDefin.txt
+
+  使用EventIdGen跑一下，得到EventId.
+
+3. 将db签入到rpg.BI_ActEventDefine
+
+insert into ButtonIdDefine(ButtonId,Define)values('11474','开学狂欢特惠专场_输出A星亚比军团蛋_总数');
+
+脚本： rpglog
+
+eventId=24784
+
+4. ButtonId:
+
+LogHandler.putLogCommand(new ButtonLog(BUTTON_IDS_SHOP_ITEM[index]));
+
+脚本:   rpglog
+
+select buttonId,value@count from rpglog.ButtonLog where buttonId in (#buttonId#)
+buttonId=11833
