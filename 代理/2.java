d5c2e4cb96d8894481b20d50c6e03cc12427d33c
unknown <weijuntao@rd.com>
2018-04-13 06:23:26 +0800

    update 2018041301

--- 代理/2.java
+++ 代理/2.java
@@ -20,3 +20,27 @@
 
   1) 动态代理的： 程序运行时运用反射机制动态创建的.
      动态代理类的字节码在程序运行时由java反射机制动态生成的.
+
+5. 使用 Proxy.newProxyInstance()方法创建动态代理。 newProxyInstance()方法有三个参数：
+   1) 类加载器（ClassLoader）用来加载动态代理类.
+   2) 一个要实现的接口的数组.
+   3) 一个 InvocationHandler 把所有方法的调用都转到代理上.
+
+   这个方法创建的动态代理对象包含一个 InvocationHandler 接口的的动态实现, 所有对proxy的调用都被转向到实现了 InvocationHandler 接口的 handler 上.
+
+6.
+public interface InvocationHandler{
+  Object invoke(Object proxy, Method method, Object[] args)
+         throws Throwable;
+}
+传入 invoke()方法中的 proxy 参数是实现要代理接口的动态代理对象, 通常你是不需要它的.
+invoke()方法中的 Method 对象参数代表了被动态代理的接口中要调用的方法，从这个 method 对象中你可以获取到这个方法名字，方法的参数，参数类型等等信息。
+Object 数组参数包含了被动态代理的方法需要的方法参数。注意：原生数据类型（如int，long等等）方法参数传入等价的包装对象（如Integer， Long等等）。
+
+   Java 动态代理:
+      http://wiki.jikexueyuan.com/project/java-reflection/java-dynamic.html
+
+      http://ifeve.com/java-reflection-11-dynamic-proxies/
+
+7. java反射指南:
+   http://ifeve.com/java-reflection-tutorial/
---
+++ 反射/2.java
@@ -0,0 +1,15 @@
+1. 实现的接口:  Class[] interfaces = clazz.getInterfaces();
+
+2. 修饰符对应的类： java.lang.reflect.Modifier;
+
+3. 包类: java.lang.Package;
+
+4. 构造器类: java.lang.reflect.Constructor;
+
+5. 函数类: java.lang.reflect.Method;
+
+6. 属性类:  java.lang.reflect.Field;
+
+7. 注解类: java.lang.annotation.Annotation
+
+8. 数组:  java.lang.reflect.Array;
