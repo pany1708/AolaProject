2050b593f9330edfb0fa6b5c16b6da32dd0368cb
unknown <weijuntao@rd.com>
2018-05-15 21:09:09 +0800

    update 2018051501

--- java.io/NIO/Selector.java
+++ java.io/NIO/Selector.java
@@ -2,4 +2,11 @@
 
 2. NIO引入了选择器的概念, 选择器用于监听多个通道的事件[比如: 连接打开, 数据到达]. ====>>> 单个线程可以监听多个数据通道.
 
-3. 
+3. 创建:
+Selector selector = Selector.open();
+
+4. Channel和Selector配合使用，必须将channel注册到selector上:
+ServerSocketChannelconfigureBlocking(false);
+ServerSocketChannel.register(selector, SelectionKey.OP_ACCEPT);
+
+5. 与Selector一起使用时,Channel必须处于非阻塞状态下.
