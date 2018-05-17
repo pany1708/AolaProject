bc8fdb2ae72b73637fdf0becfa2f2fa96b71827e
unknown <weijuntao@rd.com>
2018-05-17 23:12:03 +0800

    update 208051701

--- java.io/NIO/Selector.java
+++ java.io/NIO/Selector.java
@@ -4,9 +4,25 @@
 
 3. 创建:
 Selector selector = Selector.open();
+一创建就会处于打开状态.
 
 4. Channel和Selector配合使用，必须将channel注册到selector上:
 ServerSocketChannelconfigureBlocking(false);
 ServerSocketChannel.register(selector, SelectionKey.OP_ACCEPT);
 
 5. 与Selector一起使用时,Channel必须处于非阻塞状态下.
+
+6. NIO能通过单个线程管理多个 I/O 通道, 主要是通过选择器Selector来实现的.
+
+7. 将通道注册到选择器Selector上时,会返回一个SelectionKey选择键对象,这个键对象标识了通道和选择器之间的注册关系.
+   1) 选择键对象会记住你关心的通道.
+   2) 会追踪到对应的通道是否已经就绪.
+   3) 调用选择器的select()时,相关的键会被更新.用来检查所有被注册到选择器的通道.
+
+   实际上就是将(channel,selector)封装成了一个SelectionKey对象，并将此对象保存在了Selector对象中。
+
+8. close(): 当调用了Selector对象的close()方法，就进入关闭状态.
+  用完Selector后调用其close()方法会关闭该Selector，且使注册到该Selector上的所有SelectionKey实例无效.
+  通道本身并不会关闭.
+
+9. 只要ServerSocketChannel及SocketChannel向Selector注册了特定的事件，Selector就会监控这些事件是否发生。
--- java.io/NIO/ServerSocketChannel.java
+++ java.io/NIO/ServerSocketChannel.java
@@ -6,4 +6,21 @@ serverSocketChannel.configureBlocking(false);//使通道为非阻塞
 ServerSocket serverSocket = ssc.socket();//创建基于NIO通道的socket连接
 serverSocket.bind(new InetSocketAddress("127.0.0.1",SERVERPORT));//新建socket通道的端口
 
-3. 
+3. 一个基于通道的socket监听器.
+
+    serverSocketChannel = ServerSocketChannel.open();
+		serverSocketChannel.configureBlocking(false);
+		// setReuseAddress应在bind之前调用才有效,其作用为:允许绑定处于TIME_WAIT 状态的端口
+		serverSocketChannel.socket().setReuseAddress(true);
+		serverSocketChannel.socket().bind(new InetSocketAddress(ConfigData.SERVER_PORT));
+
+		acceptSelector = Selector.open();
+		serverSocketChannel.register(acceptSelector, SelectionKey.OP_ACCEPT);
+
+4. 取出对等的socket设置对应socket选项.
+
+5. 2种方式创建SocketChannel：
+打开一个SocketChannel并连接到互联网上的某台服务器。
+一个新连接到达ServerSocketChannel时，会创建一个SocketChannel。
+
+6.
