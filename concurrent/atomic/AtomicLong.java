8ef61cbb53b174edc556d55a5e2f2f828e75d1b7
unknown <weijuntao@rd.com>
2018-05-22 21:30:06 +0800

    update 20180522

---
+++ concurrent/atomic/AtomicLong.java
@@ -0,0 +1,5 @@
+1. jdk新增的LongAdder和DoubleAdder, 在多线程下, 原子实现比AtomicInteger和AtomicLong提供了更优越的性能.
+
+2. 在多线程竞争下, LongAdder 性能提升明显.
+
+3. 
--- stream/3.java
+++ stream/3.java
@@ -1,4 +1,7 @@
 1. SAM接口：
 Runnable task = () -> {};
 
-2.
+2. flatMap 和 map 区别：
+https://www.jianshu.com/p/7e8b57d2d74a
+
+3. 万能的reduce():
